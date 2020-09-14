c64a6085a1c388e23a3721a06755c39d64360c48
dbdmsdn10 <dbdmsdn10@gmail.com>
2020-09-14 14:24:22 +0900

    onclick toast배움(short와 length)가 있음 eclipse설치

--- One/app/src/main/java/com/example/myapplication/MainActivity.java
+++ One/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("타이틀");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);//바탕화면에 뭔가 보여줄것인지 확인
+        getSupportActionBar().setIcon(R.drawable.icon);//이미지삽입
+
+
+    }
+
+    public void doit(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Toast.makeText(this,"버튼1",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this,"버튼2",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
