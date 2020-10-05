b075ce189c6f74837146900b4dba820472129f06
dbdmsdn10 <dbdmsdn10@gmail.com>
2020-10-05 14:18:30 +0900

    계산기 try catch 문자열.trim.equals("")  값이 없을때
    radio버튼등 각종 버튼

--- carcul/app/src/main/java/com/example/myapplication/MainActivity.java
+++ carcul/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,21 +8,25 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-TextView txtresult;
-EditText edit1,edit2;
-Button a;
+    TextView txtresult;
+    EditText edit1, edit2;
+    Button a;
+    CheckBox b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("계산기");
         setContentView(R.layout.activity_main);
-        txtresult=findViewById(R.id.txtretult);
-        edit1=findViewById(R.id.edit1);
-        edit2=findViewById(R.id.edit2);
+        txtresult = findViewById(R.id.txtretult);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
 //        a=findViewById(R.id.btnadd);
 //        a.setOnTouchListener(new View.OnTouchListener(){
 //            public boolean onTouch(View arg0,MotionEvent arg1){
@@ -32,26 +36,37 @@ Button a;
 //                return false;
 //            }
 //        });
+        b.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+            }
+        });
     }
 
     public void doit(View view) {
-        int num1=Integer.parseInt(edit1.getText().toString());
-        int num2=Integer.parseInt(edit2.getText().toString());
-        switch(view.getId()){
-            case R.id.btnadd:
-                txtresult.setText((num1+num2)+"");
-                break;
-            case R.id.btnsub:
-                txtresult.setText((num1-num2)+"");
-                break;
+        try {
+            double num1 = Double.parseDouble(edit1.getText().toString());
+            double num2 = Double.parseDouble(edit2.getText().toString());
+            switch (view.getId()) {
+                case R.id.btnadd:
+                    txtresult.setText((num1 + num2) + "");
+                    break;
+                case R.id.btnsub:
+                    txtresult.setText((num1 - num2) + "");
+                    break;
 
-            case R.id.btnmul:
-                txtresult.setText((num1*num2)+"");
-                break;
-            case R.id.btndiv:
-                txtresult.setText((num1/num2)+"");
-                break;
-
+                case R.id.btnmul:
+                    txtresult.setText((num1 * num2) + "");
+                    break;
+                case R.id.btndiv:
+                    txtresult.setText((num1 / num2) + "");
+                    break;
+            }
+        } catch (Exception e) {
+            //edit1.getText().toString().trim().equals("")
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
+
     }
 }
