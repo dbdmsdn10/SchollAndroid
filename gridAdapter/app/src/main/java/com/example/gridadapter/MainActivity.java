0298a75fc9efffd09aa6d11af2c2c3fb8372bed4
dbdmsdn10 <dbdmsdn10@gmail.com>
2020-11-09 14:58:10 +0900

    gallery-> 왼쪽 오른쪽 드래그 가능 grid-> 4*x 그림표 menu->위에 점 3개짜리 매뉴

---
+++ gridAdapter/app/src/main/java/com/example/gridadapter/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.gridadapter;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("그리드뷰 영화 포스터");
+
+        GridView gv = findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+        Integer[] posterID = {R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10
+                ,R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10};
+
+        public MyGridAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView=new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(200,300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5,5,5,5);
+            imageView.setImageResource(posterID[position]);
+            final int pos=position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogview=View.inflate(
+                            MainActivity.this,R.layout.dialog,null
+                    );
+                    ImageView ivposter=(ImageView) dialogview.findViewById(R.id.ivPoster);
+                    ivposter.setImageResource(posterID[pos]);
+
+                    AlertDialog.Builder dlg=new AlertDialog.Builder(MainActivity.this);
+                    dlg.setView(dialogview);
+                    dlg.setTitle("큰그림");
+                    dlg.setNegativeButton("닫기",null);
+                    dlg.show();
+                }
+            });
+            return imageView;
+        }
+    }
+}
